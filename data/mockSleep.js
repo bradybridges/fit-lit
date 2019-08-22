adf3a64ca786293b31f82ed93b7c60791a4c5f35
Amy Rippeto <amyrippeto@gmail.com>
2019-08-22 13:30:27 -0600

    Delete extra mock sleep data file

--- data/mockSleep.js
+++ data/mockSleep.js
@@ -1,51 +1,274 @@
 const sleepData = [
   {
     "userID": 1,
-    "date": "2019/06/15",
-    "hoursSlept": 6.1,
-    "sleepQuality": 2.2
+    "date": "2019/07/08",
+    "hoursSlept": 1,
+    "sleepQuality": 1
   },
   {
     "userID": 2,
-    "date": "2019/06/15",
-    "hoursSlept": 7,
-    "sleepQuality": 4.7
+    "date": "2019/07/08",
+    "hoursSlept": 1,
+    "sleepQuality": 1
   },
   {
     "userID": 3,
-    "date": "2019/06/15",
-    "hoursSlept": 10.8,
-    "sleepQuality": 4.7
+    "date": "2019/07/08",
+    "hoursSlept": 1,
+    "sleepQuality": 1
   },
   {
     "userID": 4,
-    "date": "2019/06/15",
-    "hoursSlept": 10.8,
+    "date": "2019/07/08",
+    "hoursSlept": 1,
+    "sleepQuality": 1
+  },
+  {
+    "userID": 5,
+    "date": "2019/07/08",
+    "hoursSlept": 1,
+    "sleepQuality": 1
+  },
+  {
+    "userID": 1,
+    "date": "2019/07/09",
+    "hoursSlept": 2,
+    "sleepQuality": 2
+  },
+  {
+    "userID": 2,
+    "date": "2019/07/09",
+    "hoursSlept": 2,
+    "sleepQuality": 2
+  },
+  {
+    "userID": 3,
+    "date": "2019/07/09",
+    "hoursSlept": 2,
+    "sleepQuality": 2
+  },
+  {
+    "userID": 4,
+    "date": "2019/07/09",
+    "hoursSlept": 2,
+    "sleepQuality": 2
+  },
+  {
+    "userID": 5,
+    "date": "2019/07/09",
+    "hoursSlept": 2,
+    "sleepQuality": 2
+  },
+  {
+    "userID": 1,
+    "date": "2019/07/10",
+    "hoursSlept": 3,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 2,
+    "date": "2019/07/10",
+    "hoursSlept": 3,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 3,
+    "date": "2019/07/10",
+    "hoursSlept": 3,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 4,
+    "date": "2019/07/10",
+    "hoursSlept": 3,
     "sleepQuality": 3
   },
   {
     "userID": 5,
-    "date": "2019/06/15",
-    "hoursSlept": 4.1,
-    "sleepQuality": 3.6
+    "date": "2019/07/10",
+    "hoursSlept": 3,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 1,
+    "date": "2019/07/11",
+    "hoursSlept": 4,
+    "sleepQuality": 4
+  },
+  {
+    "userID": 2,
+    "date": "2019/07/11",
+    "hoursSlept": 4,
+    "sleepQuality": 4
+  },
+  {
+    "userID": 3,
+    "date": "2019/07/11",
+    "hoursSlept": 4,
+    "sleepQuality": 4
+  },
+  {
+    "userID": 4,
+    "date": "2019/07/11",
+    "hoursSlept": 4,
+    "sleepQuality": 4
+  },
+  {
+    "userID": 5,
+    "date": "2019/07/11",
+    "hoursSlept": 4,
+    "sleepQuality": 4
+  },
+  {
+    "userID": 1,
+    "date": "2019/07/12",
+    "hoursSlept": 5,
+    "sleepQuality": 5
+  },
+  {
+    "userID": 2,
+    "date": "2019/07/12",
+    "hoursSlept": 5,
+    "sleepQuality": 5
+  },
+  {
+    "userID": 3,
+    "date": "2019/07/12",
+    "hoursSlept": 5,
+    "sleepQuality": 5
+  },
+  {
+    "userID": 4,
+    "date": "2019/07/12",
+    "hoursSlept": 5,
+    "sleepQuality": 5
+  },
+  {
+    "userID": 5,
+    "date": "2019/07/12",
+    "hoursSlept": 5,
+    "sleepQuality": 5
+  },
+  {
+    "userID": 1,
+    "date": "2019/07/13",
+    "hoursSlept": 6,
+    "sleepQuality": 6
+  },
+  {
+    "userID": 2,
+    "date": "2019/07/13",
+    "hoursSlept": 6,
+    "sleepQuality": 6
+  },
+  {
+    "userID": 3,
+    "date": "2019/07/13",
+    "hoursSlept": 6,
+    "sleepQuality": 6
+  },
+  {
+    "userID": 4,
+    "date": "2019/07/13",
+    "hoursSlept": 6,
+    "sleepQuality": 6
+  },
+  {
+    "userID": 5,
+    "date": "2019/07/13",
+    "hoursSlept": 6,
+    "sleepQuality": 6
+  },
+  {
+    "userID": 1,
+    "date": "2019/07/14",
+    "hoursSlept": 7,
+    "sleepQuality": 7
+  },
+  {
+    "userID": 2,
+    "date": "2019/07/14",
+    "hoursSlept": 7,
+    "sleepQuality": 7
+  },
+  {
+    "userID": 3,
+    "date": "2019/07/14",
+    "hoursSlept": 7,
+    "sleepQuality": 7
+  },
+  {
+    "userID": 4,
+    "date": "2019/07/14",
+    "hoursSlept": 7,
+    "sleepQuality": 7
+  },
+  {
+    "userID": 5,
+    "date": "2019/07/14",
+    "hoursSlept": 7,
+    "sleepQuality": 7
   },
   {
     "userID": 1,
     "date": "2019/07/15",
-    "hoursSlept": 6.1,
-    "sleepQuality": 2.2
+    "hoursSlept": 8,
+    "sleepQuality": 8
+  },
+  {
+    "userID": 2,
+    "date": "2019/07/15",
+    "hoursSlept": 8,
+    "sleepQuality": 8
+  },
+  {
+    "userID": 3,
+    "date": "2019/07/15",
+    "hoursSlept": 8,
+    "sleepQuality": 8
+  },
+  {
+    "userID": 4,
+    "date": "2019/07/15",
+    "hoursSlept": 8,
+    "sleepQuality": 8
+  },
+  {
+    "userID": 5,
+    "date": "2019/07/15",
+    "hoursSlept": 8,
+    "sleepQuality": 8
   },
   {
     "userID": 1,
-    "date": "2019/08/15",
-    "hoursSlept": 6.1,
-    "sleepQuality": 2.2
+    "date": "2019/07/16",
+    "hoursSlept": 9,
+    "sleepQuality": 9
   },
   {
-    "userID": 1,
-    "date": "2019/09/15",
-    "hoursSlept": 6.1,
-    "sleepQuality": 2.2
-  },];
+    "userID": 2,
+    "date": "2019/07/16",
+    "hoursSlept": 9,
+    "sleepQuality": 9
+  },
+  {
+    "userID": 3,
+    "date": "2019/07/16",
+    "hoursSlept": 9,
+    "sleepQuality": 9
+  },
+  {
+    "userID": 4,
+    "date": "2019/07/16",
+    "hoursSlept": 9,
+    "sleepQuality": 9
+  },
+  {
+    "userID": 5,
+    "date": "2019/07/16",
+    "hoursSlept": 9,
+    "sleepQuality": 9
+  },
+  ];
 
-  module.exports = sleepData;
+  module.exports = sleepData;  
