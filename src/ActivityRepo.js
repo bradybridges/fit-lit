07ea7d5ca05ad84c59a297216dc1e71b5a91d568
Amy Rippeto <amyrippeto@gmail.com>
2019-08-21 21:11:44 -0600

    Add tests and methods for activityRepo

--- src/ActivityRepo.js
+++ src/ActivityRepo.js
@@ -8,7 +8,33 @@ class ActivityRepo {
     return this.activityData.filter(data => data.userID === id);
   }
 
-  
+  getTodaysData(date) {
+    return this.activityData.filter(data => data.date === date);
+  }
+
+  getAvgStairsAllUsers(date) {
+    const todaysData = this.getTodaysData(date);
+    return +(todaysData.reduce((total, data) => {
+      total += data.flightsOfStairs;
+      return total;
+    }, 0) / todaysData.length).toFixed(2);
+  }
+
+  getAvgStepsAllUsers(date) {
+    const todaysData = this.getTodaysData(date);
+    return +(todaysData.reduce((total, data) => {
+      total += data.numSteps;
+      return total;
+    }, 0) / todaysData.length).toFixed(2);
+  }
+
+  getAvgMinActiveAllUsers(date) {
+    const todaysData = this.getTodaysData(date);
+    return +(todaysData.reduce((total, data) => {
+      total += data.minutesActive;
+      return total;
+    }, 0) / todaysData.length).toFixed(2);    
+  }
 
   
 }
--- test/ActivityRepo-test.js
+++ test/ActivityRepo-test.js
@@ -2,20 +2,39 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const activityData = require('../data/mockActivity');
+const userData = require('../data/mockUsers');
 const ActivityRepo = require('../src/ActivityRepo');
+const UserRepo = require('../src/UserRepo');
 
 describe('ActivityRepo', () => {
 
-  let activityRepo, date;
+  let activityRepo, users, date;
 
   beforeEach(() => {
-    activityRepo = new ActivityRepo(activityData);
+    users = new UserRepo(userData);
+    activityRepo = new ActivityRepo(activityData, users);
     date = '2019/07/15';
   })
 
   it('should return user activity given id', () => {
     expect(activityRepo.getUserActivity(1).length).to.equal(9);
   })
+
+  it('should return all users avg stairs given date', () => {
+    expect(activityRepo.getAvgStairsAllUsers(date)).to.equal(25.4);
+  })
+
+  it('should return all users avg steps given date', () => {
+    expect(activityRepo.getAvgStepsAllUsers(date)).to.equal(9520);
+  })
+
+  it('should return all users avg min active given date', () => {
+    expect(activityRepo.getAvgMinActiveAllUsers(date)).to.equal(204);
+  })
+
+  // it('should   ', () => {
+  //   // make one
+  // })
 
 
 })
