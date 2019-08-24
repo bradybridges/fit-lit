89cc961f2a7deb66259c61ed9afd9e4d766dbb13
Brady Bridges <bradyjbridges@gmail.com>
2019-08-24 15:50:14 -0600

    Append most data to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -13,5 +13,103 @@ $(document).ready(() =>{
   sleep = new Sleep(sleepRepo.getUserData(id));
   activity = new Activity(activityRepo.getUserActivity(id), userRepo.getUser(id));
 
-  $('#first-name').text(user.getFirstName())
-})
+  //functions
+  //user details fns
+  const getUserID = () => {
+    return user.id;
+  }
+
+  const getFullName = () => {
+    return user.name;
+  }
+
+  const getAddress = () => {
+    return user.address;
+  }
+
+  const getEmail = () => {
+    return user.email;
+  }
+
+  const getStrideLength = () => {
+    return user.strideLength;
+  }
+
+  const getDailyStepGoal = () => {
+    return user.dailyStepGoal;
+  }
+
+  const getAverageStepGoal = () => {
+    return userRepo.calculateAverageStepGoal();
+  }
+
+  const createFriendNamesList = () => {
+    const friendIDs = user.friends;
+    let friendNames = '';
+    friendIDs.forEach(friendID => {
+      let friendName = userRepo.getUser(friendID).name;
+      friendNames += `<li class='friend'>${friendName}</li>`;
+    });
+    return friendNames;
+  }
+
+  //hydration fns
+  const getWaterToday = () => {
+    return `${hydration.getFluidOuncesOnDay(date)} Ounces`;
+  }
+
+  const getAverageWaterConsumtion = () => {
+    return `${hydration.getAverageFluidOunces()} Ounces`;
+  }
+
+  //sleep fns
+  const getHoursSlept = () => {
+    return `${sleep.getHoursSlept(date)} Hours`;
+  }
+
+  const getAvgHoursSlept = () => {
+    return `${sleep.calculateAverageHoursSlept()} Hours`;
+  }
+
+  const getSleepQuality = () => {
+    return sleep.getQuality(date);
+  }
+
+  const getAvgSleepQuality = () => {
+    return sleep.calculateAverageSleepQuality();
+  }
+
+  //activity fns
+  const getMilesWalkedToday = () => {
+    return activity.getMilesWalked(date);
+  }
+
+  //header
+  $('#first-name').text(user.getFirstName());
+  $('#date').text(date);
+
+  //main
+  //user details
+  $('#user-id').text(getUserID());
+  $('#full-name').text(getFullName());
+  $('#address').text(getAddress());
+  $('#email').text(getEmail());
+  $('#stride').text(getStrideLength());
+  $('#step-goal').text(getDailyStepGoal());
+  $('#step-goal-avg').text(getAverageStepGoal());
+  $('#friends').append(createFriendNamesList());
+
+  //hydration
+  $('#water-today').text(getWaterToday());
+  $('#water-average').text(getAverageWaterConsumtion());
+
+  //sleep
+  $('#hours-sleep').text(getHoursSlept());
+  $('#avg-hours').text(getAvgHoursSlept());
+  $('#quality-sleep').text(getSleepQuality());
+  $('#avg-quality').text(getAvgSleepQuality());
+
+  //activity
+  $('#miles').text(getMilesWalkedToday());
+
+});
